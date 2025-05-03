69ebe70bd0d3c2f184995f30c2713233711a4d04
hamidcass <152835384+hamidcass@users.noreply.github.com>
2025-05-03 11:03:11 -0400

    starrt to device page

--- frontend/template/src/_nav.js
+++ frontend/template/src/_nav.js
@@ -19,12 +19,11 @@ import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 const _nav = [
   {
     component: CNavItem,
-    name: 'Dashboard',
-    to: '/dashboard',
+    name: 'Devices',
+    to: '/devices',
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
     badge: {
       color: 'info',
-      text: 'NEW',
     },
   },
   {
